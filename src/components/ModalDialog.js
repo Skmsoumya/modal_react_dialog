c3589d66b8784a73742a27187f71583d866f7de1
Skmsoumya <soumyakishormishra@gmail.com>
2017-11-20 17:16:35 +0530

    initial commit with modalDialogComponent

---
+++ src/components/ModalDialog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./ModalDialog.css"
+
+class ModalDialog extends Component {
+	render() {
+		return (
+			<div class="dialog">
+				This is a modal dialog
+			</div>
+		);
+	}
+}
+
+export default ModalDialog;
